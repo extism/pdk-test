26255a2297e9699b45a079eaca564b9db263926d
zach <zach@dylibso.com>
2024-06-13 17:00:20 -0700

    wip: run against CLI too

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::process::exit;
+use std::process::{exit, Stdio};
 
 pub fn main() {
     let args: Vec<_> = std::env::args().skip(1).collect();
     let mut imports = Default::default();
+    let mut errors = 0;
     for arg in args {
         let data = std::fs::read(&arg).unwrap();
 
@@ -13,13 +14,45 @@ pub fn main() {
         .with_wasi(true)
         .build()
         .unwrap();
+
         if plugin.function_exists("kitchen_sink") {
-            let output: &str = plugin.call("kitchen_sink", "test").unwrap();
-            assert_eq!(output, "test");
+            println!("INFO Calling kitchen_sink using the Rust SDK");
+            let output: Result<&str, extism::Error> = plugin.call("kitchen_sink", "test");
+            match output {
+                Ok(output) => {
+                    if output != "test" {
+                        println!("ERROR invalid results from kitchen_sink");
+                        errors += 1
+                    }
+                }
+                Err(e) => {
+                    println!("ERROR kitchen_sink:");
+                    println!("{:?}", e);
+                    errors += 1;
+                }
+            }
+
+            println!("INFO Calling kitchen_sink using the Go SDK/CLI");
+            if std::process::Command::new("extism")
+                .arg("call")
+                .arg(&arg)
+                .arg("kitchen_sink")
+                .arg("--allow-host")
+                .arg("extism.org")
+                .arg("--wasi")
+                .stdout(Stdio::null())
+                .status()
+                .is_err()
+            {
+                println!("ERROR unable to call kitchen_sink using extism CLI");
+                errors += 1;
+            }
+        } else {
+            println!("WARNING kitchen_sink function not found");
         }
     }
-    let mut errors = 0;
 
+    println!("INFO Checking for Extism functions");
     for found in &imports.found {
         println!("FOUND extism:host/env::{}", found);
     }
